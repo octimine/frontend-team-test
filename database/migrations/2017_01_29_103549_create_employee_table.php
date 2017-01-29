f88e776d266a92998f2c5acfc2ed084b323558d2
Harisankar <mrsank@gmail.com>
2017-01-29 11:43:08 +0100

    + Employee table schema created

--- database/migrations/2017_01_29_103549_create_employee_table.php
+++ database/migrations/2017_01_29_103549_create_employee_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateEmployeeTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        // Create employee table
+
+        Schema::create('employee', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('employee_id');
             $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('branch');
             $table->timestamps();
         });
     }
@@ -30,6 +32,8 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        // Drop employee table
+
+        Schema::dropIfExists('employee');
     }
 }
